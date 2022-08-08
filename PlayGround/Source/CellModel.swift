fddc49c416881e8ef8ce066fbb4d7f25e72a8895
meltsplit <seokwoo2000@naver.com>
2022-08-08 22:58:17 +0900

    TableView Cell 생명주기. 재사용 이슈 해결

---
+++ PlayGround/Source/CellModel.swift
@@ -0,0 +1,15 @@
+//
+//  CellModel.swift
+//  PlayGround
+//
+//  Created by 장석우 on 2022/08/07.
+//
+
+import Foundation
+
+struct CellModel{
+    
+    let text : String
+    var isSwitchOn : Bool = true
+    
+}
